d748aa067143c5d4e4ff21b6bf0c2eedaf17b6c6
masudrahman-dev <masudrahman.dev@gmail>
2023-05-02 00:22:19 +0600

    get specific url data

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const categories = require("./data/categories.json");
-const news = require("./data/news.json");
+const allNews = require("./data/news.json");
+app.get("/", (req, res) => {
+  res.send("Hello World! I am here");
+});
 app.get("/categories", (req, res) => {
   // res.send('Hello World! I am here')
   res.send(categories);
   //   res.json(categories);
 });
-app.get("/news", (req, res) => {
+app.get("/allNews", (req, res) => {
+  res.send(allNews);
+});
+app.get("/allNews/:id", (req, res) => {
+  // res.send(news);
+  const news = allNews.find((news) => news.category_id === req.params.id);
+  console.log(news);
   res.send(news);
 });
 
